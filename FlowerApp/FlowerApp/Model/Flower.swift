4f317bbe3b44007c43c849df702b8f9cb8277cb0
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2019-09-09 17:51:12 -0300

    Creating Flower and Main Interface

---
+++ FlowerApp/FlowerApp/Model/Flower.swift
@@ -0,0 +1,16 @@
+//
+//  Flower.swift
+//  FlowerApp
+//
+//  Created by Gabriel on 09/09/19.
+//  Copyright © 2019 gabrielrodrigues. All rights reserved.
+//
+
+import Foundation
+
+class Flower: Decodable {
+    var name: String?
+    var description: String?
+    var latinName: String?
+    var pictureUrl: String?
+}
